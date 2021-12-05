420226250e5a0da05f2d3fae3629c2555724b4ec
July Moss <july.x.moss@gmail.com>
2021-12-05 10:51:30 +0000

    completed all diy-kata test.js files

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
+describe ("humanCatDogYears", () =>{
+    it ("return an array of three numbers represent human cat and dog ages", () =>{
+        expect (humanCatDogYears(10)).toEqual([10, 56, 64]);
+        expect (humanCatDogYears(18)).toEqual([18, 88, 104]);
+    })
+})
 // Look Ma, no handlebars!!!
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,8 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it ("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}])).toEqual('Bart, Lisa & Maggie');
+    expect(joinNames([{name: 'July'}, {name: 'Lisa'}, {name: 'Harry'}, {name: 'Joe'}, {name: 'Dan'}])).toEqual('July, Lisa, Harry, Joe & Dan');    
+  });
 });
